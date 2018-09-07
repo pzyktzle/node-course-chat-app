677585ccc1ea5a918ce4f14cc84e79b3741dfc62
Agey <pzyktzle@gmail.com>
2018-09-07 16:02:45 +1000

    Emit newMessage on createMessage

--- server/server.js
+++ server/server.js
@@ -23,31 +23,15 @@ app.use(express.static(path.join(__dirname, '../public')));
 io.on('connection', (socket) => {
   console.log('New user connected to server');
 
-  // // server -> client
-  // socket.emit('newEmail', {
-  //   from: 'agey@agey.io',
-  //   text: 'hey',
-  //   createdAt: 123
-  // });
-
-  // client -> server
   socket.on('createMessage', (message) => {
     console.log('createMessage', message);
+    io.emit('newMessage', {
+      from: message.from,
+      text: message.text,
+      createdAt: Date.now()
+    });
   });
 
-  // server -> client
-  socket.emit('newMessage', {
-    from: 'pzyktzle',
-    text: 'hey',
-    createdAt: 123
-  });
-
-  // // client -> server
-  // socket.on('createEmail', (newEmail) => {
-  //   console.log('createEmail', newEmail);
-  // });
-
-  // client -> server
   socket.on('disconnect', () => {
     console.log('Client disconnected');
   });
