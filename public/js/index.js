06acb10795a2a1d8facc88977fb91667fbb02bb6
Agey <pzyktzle@gmail.com>
2018-09-08 15:41:44 +1000

    Add css for chat page

--- public/js/index.js
+++ public/js/index.js
@@ -30,11 +30,22 @@ socket.on('newLocationMessage', function (message) {
 jQuery('#message-form').on('submit', function (e) {
   e.preventDefault();
 
+  var messageTextbox = jQuery('[name=message]');
+  var messageButton = jQuery('#send-message');
+
+  if (messageTextbox.val() === '') {
+    return;
+  }
+
+  messageButton.attr('disabled', 'disabled');
+
   socket.emit('createMessage', {
     from: 'User',
-    text: jQuery('[name=message]').val()
+    text: messageTextbox.val()
   }, function () {
-
+    messageTextbox.val(''); // clears text when this callback is fired (acknowlegment from the server)
+    messageTextbox.focus();
+    messageButton.removeAttr('disabled');
   });
 })
 
@@ -44,12 +55,17 @@ locationButton.on('click', function () {
     return alert('Geolocation not supported by your browser');
   }
 
+locationButton.attr('disabled', 'disabled').text('Sending location ...');
+
   navigator.geolocation.getCurrentPosition(function (position) {
+    locationButton.removeAttr('disabled').text('Send location');
+
     socket.emit('createLocationMessage', {
       latitude: position.coords.latitude,
       longitude: position.coords.longitude
     });
   }, function () {
+    locationButton.removeAttr('disabled').text('Send location');
     alert('Unable to fetch location');
   });
 });
