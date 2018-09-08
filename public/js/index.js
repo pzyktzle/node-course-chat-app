c1f3972e3ef67ffffb7a0824d6e5e95a24883945
Agey <pzyktzle@gmail.com>
2018-09-08 18:51:22 +1000

    Add mustachejs for message templates

--- public/js/index.js
+++ public/js/index.js
@@ -13,10 +13,13 @@ socket.on('disconnect', function () {
 //
 socket.on('newMessage', function (message) {
   var formattedTime = moment(message.createdAt).format('h:mm a');
-
-  var li = jQuery('<li></li>');
-  li.text(`${message.from} ${formattedTime}: ${message.text}`);
-  jQuery('#messages').append(li);
+  var template = jQuery('#message-template').html();
+  var html = Mustache.render(template, {
+    text: message.text,
+    from: message.from,
+    createdAt: formattedTime
+  });
+  jQuery('#messages').append(html);
 });
 
 //
@@ -24,26 +27,24 @@ socket.on('newMessage', function (message) {
 //
 socket.on('newLocationMessage', function (message) {
   var formattedTime = moment(message.createdAt).format('h:mm a');
-  var li = jQuery('<li></li>');
-  var a = jQuery('<a target="_blank">My current location</a>')
+  var template = jQuery('#location-message-template').html();
+  var html = Mustache.render(template, {
+    from: message.from,
+    url: message.url,
+    createdAt: formattedTime
+  });
+  jQuery('#messages').append(html);
 
-  li.text(`${message.from} ${formattedTime}: `);
-  a.attr('href', message.url);
-  li.append(a);
-
-  jQuery('#messages').append(li);
 });
 
 jQuery('#message-form').on('submit', function (e) {
   e.preventDefault();
-
   var messageTextbox = jQuery('[name=message]');
   var messageButton = jQuery('#send-message');
 
   if (messageTextbox.val() === '') {
     return;
   }
-
   messageButton.attr('disabled', 'disabled');
 
   socket.emit('createMessage', {
